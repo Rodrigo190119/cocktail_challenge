4a7389caffea6f0988622a864d323c3c681c0e01
Rodrigo190119 <ra.molerocaceda1901@gmail.com>
2022-12-21 01:31:24 -0500

    deleted unused image

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,17 @@ import 'package:cocktail_challenge/data/presenter/login_presenter.dart';
 import 'package:cocktail_challenge/ui/views/login/login_view.dart';
 import 'package:cocktail_challenge/utils/preferences_util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Prefs.init();
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown
+  ]).whenComplete(() => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
